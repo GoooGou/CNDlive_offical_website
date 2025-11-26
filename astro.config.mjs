7ef7287f48ca133e1136ac86d69e305ae44ae46a
GoooGou <162654668+GoooGou@users.noreply.github.com>
2025-11-26 17:18:58 +0800

    域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,48 +12,52 @@ import partytown from '@astrojs/partytown';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-site: 'http://localhost:4321',
-  integrations: [// 1. Starlight 配置 (建议放在最前面)
-  // 2. React 支持
-  starlight({
-    // 🔥 必填：文档站标题
-    title: 'CNDLive Support',
-// 🔥🔥🔥 添加这一行，禁用 Starlight 默认的 404 路由 🔥🔥🔥
-    disable404Route: true,
-    
-    // 自定义 CSS 文件路径
-    customCss: [
-      // 你的自定义 CSS 文件的相对路径
-      './src/styles/starlight-simple.css',
-    ],
-    // 侧边栏配置
-    sidebar: [
-            {
-        label: '🔙 返回主站',
-        link: '/',
-        // 可选：加上 icon 让它更好看
-        // 可选：新标签页打开
-        attrs: { target: '_blank' }, 
-      },
-      // 方案 A：自动读取 src/content/docs/support 文件夹下的所有内容
-      {
-        label: 'Support Docs',
-        autogenerate: { directory: 'support' },
-      },
-      
-      // 方案 B：手动指定 (如果你想控制顺序)
-      // {
-      //   label: 'Start Here',
-      //   items: [
-      //     { label: 'Introduction', link: '/support/intro' },
-      //   ],
-      // },
-    ],
-  }), // 3. 如果你发现你的 News/About 页面无法渲染 MDX 了，再把下面这行注释解开
-  // mdx(), 
-  react(), sitemap(), partytown()],
+  site: 'http://www.pixelcool.site',
+  integrations: [
+    // 1. Starlight 配置 (建议放在最前面)
+    // 2. React 支持
+    starlight({
+      // 🔥 必填：文档站标题
+      title: 'CNDLive Support',
+      // 🔥🔥🔥 添加这一行，禁用 Starlight 默认的 404 路由 🔥🔥🔥
+      disable404Route: true,
+
+      // 自定义 CSS 文件路径
+      customCss: [
+        // 你的自定义 CSS 文件的相对路径
+        './src/styles/starlight-simple.css',
+      ],
+      // 侧边栏配置
+      sidebar: [
+        {
+          label: '🔙 返回主站',
+          link: '/',
+          // 可选：加上 icon 让它更好看
+          // 可选：新标签页打开
+          attrs: { target: '_blank' },
+        },
+        // 方案 A：自动读取 src/content/docs/support 文件夹下的所有内容
+        {
+          label: 'Support Docs',
+          autogenerate: { directory: 'support' },
+        },
+
+        // 方案 B：手动指定 (如果你想控制顺序)
+        // {
+        //   label: 'Start Here',
+        //   items: [
+        //     { label: 'Introduction', link: '/support/intro' },
+        //   ],
+        // },
+      ],
+    }), // 3. 如果你发现你的 News/About 页面无法渲染 MDX 了，再把下面这行注释解开
+    // mdx(),
+    react(),
+    sitemap(),
+    partytown(),
+  ],
 
   // 预加载策略
   prefetch: {
